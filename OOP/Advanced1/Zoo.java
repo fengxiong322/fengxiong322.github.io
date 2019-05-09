4d6624fdb22149d6ef8f67d7de54192180fdcffc
Feng Xiong <feng.xiong@student.tdsb.on.ca>
2019-05-09 19:35:34 -0400

    Added more advanced oop content
    deleted modal

---
+++ OOP/Advanced1/Zoo.java
@@ -0,0 +1,8 @@
+public class Zoo{
+  public static void main(String[] args) {
+    Animal[] zooAnimals = new Animal[3];
+    zooAnimals[0] = new Monkey();
+    zooAnimals[1] = new Tiger();
+    zooAnimals[2] = new Zebra();
+  }
+}
